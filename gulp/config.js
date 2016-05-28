346172d554b3bb8d81c5b85badb7f793cb45701c
anthonykoch <anthonykoch10@gmail.com>
2016-05-27 23:16:55 -0700

    Update browsersync file includes

--- gulp/config.js
+++ gulp/config.js
@@ -9,7 +9,7 @@ module.exports =
 		notify: false,
 		open: false,
 		injectChanges: true,
-		files: ['css'],
+		files: ['assets/stylesheets'],
 		reloadDelay: 600,
 		middleware: [require('compression')()]
 	},
